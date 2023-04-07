5d1a8b0639d3cbd8d3c7a80c8f1f2e761576fda1
Alex Baptista <86619770+atbaptista@users.noreply.github.com>
2023-04-07 15:28:52 -0400

    recording plus changes

--- prototype/server.js
+++ prototype/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-import fetch from "node-fetch";
+const fetch = require('node-fetch');
 const app = express();
 
-const apiKey = 'd2ae45b1e88742959ed68671b3217c20'; 
+const SteamAPI = require('steamapi');
+const steam = new SteamAPI('9F8C1996E787E164C62A6FB2C317AA21');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -14,24 +15,19 @@ app.get('/', (req, res) => {
 
 // define route to handle search form submission
 app.get('/search', async (req, res) => {
-  const gameName = req.query.game_name;
-  const gamePlatform = req.query.game_platform;
+  const steam_id = req.query.steam_id;
+  const userSummary = steam.getUserSummary(steam_id)
+    .then(userSummary => {
+      console.log(userSummary);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 
-  // make API request to RAWG database
-  const response = await fetch(`https://accujazz-rawg-video-games-database.p.rapidapi.com/games?search=${gameName}&platforms=${gamePlatform}`, {
-    headers: {
-      'x-rapidapi-host': 'accujazz-rawg-video-games-database.p.rapidapi.com',
-      'x-rapidapi-key': apiKey,
-      'useQueryString': true,
-      'User-Agent': 'Chrome/93.0.4577.63'
-    }
-  });
-
-  const data = await response.json();
-  const games = data.results;
-
-  // render search results template with the game data
-  res.render('search-results', { games });
+  const user_avatar = userSummary.avatar;
+  const user_name = userSummary.nickname;
+  console.log(user_avatar);
+  console.log(user_name);
 });
 
 // start server
